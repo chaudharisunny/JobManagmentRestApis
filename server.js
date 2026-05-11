536a62702e3e0226fb71792d5f512a6d75bbec50
chaudharisunny <73020065+chaudharisunny@users.noreply.github.com>
2026-05-11 13:37:17 +0530

    fix cors for vercel frontend

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ connectDB();
 app.use(express.json());
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://job-managment-frontend.vercel.app"
+  ],
   credentials: true
 }));
 
