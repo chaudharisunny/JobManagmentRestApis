86dec60fbfdad73c871ceb1a36826ad59b5f98f9
chaudharisunny <73020065+chaudharisunny@users.noreply.github.com>
2026-05-12 11:09:09 +0530

    Fix production CORS

--- server.js
+++ server.js
@@ -17,13 +17,13 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://job-managment-frontend-jset.vercel.app",
-    ],
+    origin: "https://job-managment-frontend-jset.vercel.app",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 
 app.options("*", cors());
 
